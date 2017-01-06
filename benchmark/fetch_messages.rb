1c4c90494e252d90f256d009c6c3810ff49044a2
sonots <sonots@gmail.com>
2017-01-06 14:22:15 +0900

    rps

--- benchmark/fetch_messages.rb
+++ benchmark/fetch_messages.rb
@@ -1,21 +1,30 @@
 require_relative 'client'
 require 'parallel'
 require 'optparse'
-params = ARGV.getopts('p:n:o:')
+params = ARGV.getopts('p:d:o:')
 
 num_parallels = Integer(params['p'] || 2)
-num_times = Integer(params['n'] || 10)
+duration = Integer(params['d'] || 1)
 offset = Integer(params['o'] || 0)
-puts "num_parallels: #{num_parallels}, num_times: #{num_times}, offset: #{offset}"
+triglav_url = params['u'] || 'http://localhost:7800'
+puts "-p (num_parallels) #{num_parallels} -d (duration) #{duration} -u (triglav_url) #{triglav_url} -o (offset) #{offset}"
 
-client = Client.new
-resource_uri = "hdfs://host:port/path/to/resource"
+client = Client.new(url: triglav_url)
 
 started = Time.now
-Parallel.each(1..num_parallels, in_processes: num_parallels) do |i|
-  num_times.times do
-    client.fetch_messages(offset, limit: 100, resource_uris: [resource_uri])
+counts = Parallel.map(1..num_parallels, in_processes: num_parallels) do |i|
+  count = 0
+  loop do
+    10.times do
+      count += 1
+      resource_uri = "hdfs://host:port/#{count}/path/to/resource"
+      client.fetch_messages(offset, limit: 100, resource_uris: [resource_uri])
+    end
+    elapsed = Time.now - started
+    break if elapsed > duration
   end
+  count
 end
 elapsed = Time.now - started
-puts "elapsed: #{elapsed.to_f}sec"
+puts "#{counts.inject(:+) / elapsed.to_f} request / sec"
+puts "#{counts.inject(:+) / num_parallels.to_f / elapsed.to_f} request / sec / process"
--- benchmark/send_messages.rb
+++ benchmark/send_messages.rb
@@ -1,27 +1,36 @@
 require_relative 'client'
 require 'parallel'
 require 'optparse'
-params = ARGV.getopts('p:n:')
+params = ARGV.getopts('p:d:u:')
 
 num_parallels = Integer(params['p'] || 2)
-num_times = Integer(params['n'] || 10)
-puts "num_parallels: #{num_parallels}, num_times: #{num_times}"
+duration = Integer(params['d'] || 1)
+triglav_url = params['u'] || 'http://localhost:7800'
+puts "-p num_parallels: #{num_parallels} -d duration: #{duration} -u triglav_url: #{triglav_url}"
 
-client = Client.new
-sample_event = {
-  resource_uri: "hdfs://host:port/path/to/resource",
+client = Client.new(url: triglav_url)
+event = {
+  resource_uri: "hdfs://host:port/%d/path/to/resource",
   resource_unit: 'daily',
   resource_time: Time.now.to_i,
   resource_timezone: "+09:00",
   payload: {free: "text"}.to_json,
 }
-events = [sample_event]
 
 started = Time.now
-Parallel.each(1..num_parallels, in_processes: num_parallels) do |i|
-  num_times.times do
-    client.send_messages(events)
+counts = Parallel.map(1..num_parallels, in_processes: num_parallels) do |i|
+  count = 0
+  loop do
+    10.times do
+      count += 1
+      _event = event.merge(resource_uri: "hdfs://host:port/#{count}/path/to/resource")
+      client.send_messages([_event])
+    end
+    elapsed = Time.now - started
+    break if elapsed > duration
   end
+  count
 end
 elapsed = Time.now - started
-puts "elapsed: #{elapsed.to_f}sec"
+puts "#{counts.inject(:+) / elapsed.to_f} request / sec"
+puts "#{counts.inject(:+) / num_parallels.to_f / elapsed.to_f} request / sec / process"
