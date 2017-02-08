f670a641ce62e9f79ccb820369f6dc010fa980af
sonots <sonots@gmail.com>
2017-02-08 22:34:35 +0900

    shellcheck

--- script/swagger-codegen-java.sh
+++ script/swagger-codegen-java.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#!/bin/bash -eu
 # Usage: script/swagger-codegen-java.sh /path/to/triglav-client-java/codegen
 
-APP_ROOT=$(cd $(dirname $0);cd ..;pwd)
+APP_ROOT=$(cd "$(dirname "$0")";cd ..;pwd)
 OUTPUT="$APP_ROOT/../triglav-client-java"
-if [ -n "$1" ]; then
-  OUTPUT=$1
+if [ $# -ge 1 ]; then
+  OUTPUT="$1"
 fi
 
 bundle exec rails swagger:generate
 
-java -jar ~/bin/swagger-codegen-cli.jar generate \
-  -i $APP_ROOT/tmp/swagger.json \
-  -l java \
-  -c $APP_ROOT/script/swagger-codegen-java.json \
-  -o $OUTPUT
+"rm" -f "$OUTPUT"/LICENSE
+"rm" -f "$OUTPUT"/README.md
+"rm" -f "$OUTPUT"/build.gradle
+"rm" -f "$OUTPUT"/build.sbt
+"rm" -f "$OUTPUT"/git_push.sh
+"rm" -f "$OUTPUT"/gradle.properties
+"rm" -f "$OUTPUT"/gradlew.bat
+"rm" -f "$OUTPUT"/pom.xml
+"rm" -f "$OUTPUT"/settings.gradle
 
-cd $OUTPUT && ./gradlew build
+java -jar ~/bin/swagger-codegen-cli.jar generate \
+  -i "$APP_ROOT"/tmp/swagger.json \
+  -l java \
+  -c "$APP_ROOT"/script/swagger-codegen-java.json \
+  -o "$OUTPUT"
+
+cd "$OUTPUT" && ./gradlew build
--- script/swagger-codegen-ruby.sh
+++ script/swagger-codegen-ruby.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/bash -eu
 # Usage: script/swagger-codegen-ruby.sh /path/to/triglav-client-ruby/codegen
 
-APP_ROOT=$(cd $(dirname $0);cd ..;pwd)
+APP_ROOT=$(cd "$(dirname "$0")";cd ..;pwd)
 OUTPUT="$APP_ROOT/../triglav-client-ruby"
-if [ -n "$1" ]; then
+if [ $# -ge 1 ]; then
   OUTPUT=$1
 fi
 
 bundle exec rails swagger:generate
 
 java -jar ~/bin/swagger-codegen-cli.jar generate \
-  -i $APP_ROOT/tmp/swagger.json \
+  -i "$APP_ROOT"/tmp/swagger.json \
   -l ruby \
-  -c $APP_ROOT/script/swagger-codegen-ruby.json \
-  -o $OUTPUT
+  -c "$APP_ROOT"/script/swagger-codegen-ruby.json \
+  -o "$OUTPUT"
