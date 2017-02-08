62e83de26db28fad9010d77919737bf560f6e447
sonots <sonots@gmail.com>
2017-02-08 16:12:11 +0900

    spec/models/message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -32,4 +32,54 @@ RSpec.describe Message, type: :model do
       expect(result[:num_inserts]).to eq(1)
     end
   end
+
+  describe 'build_with_job_message' do
+    let(:job) do
+      FactoryGirl.create(:job_with_single_resource)
+    end
+
+    let(:resource) do
+      job.input_resources.first
+    end
+
+    let(:message_params) {
+      {
+        resource_uri: resource.uri,
+        resource_unit: resource.unit,
+        resource_timezone: resource.timezone,
+        resource_time: 1356361200,
+        payload: '{"path":"hdfs://foo/bar","last_modification_time":1356361200000}',
+      }
+    }
+
+    it do
+      subject = Message.build_with_job_message(message_params)
+      expect(JobMessage.all.size).to be > 0
+    end
+  end
+
+  describe 'create_messages' do
+    let(:job) do
+      FactoryGirl.create(:job_with_single_resource)
+    end
+
+    let(:resource) do
+      job.input_resources.first
+    end
+
+    let(:message_params) {
+      {
+        resource_uri: resource.uri,
+        resource_unit: resource.unit,
+        resource_timezone: resource.timezone,
+        resource_time: 1356361200,
+        payload: '{"path":"hdfs://foo/bar","last_modification_time":1356361200000}',
+      }
+    }
+
+    it do
+      result = Message.create_messages([message_params])
+      expect(result[:num_inserts]).to eq(1)
+    end
+  end
 end
